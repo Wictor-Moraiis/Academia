6249fe69e33c1e857bc6f5abf718b8bc806e1bf0
Wictor Morais Gonçalves <wictor.morais2@gmail.com>
2026-02-28 00:45:44 -0300

    Criacao classe usuario
    Usuario criado, encapsulando seus parametros e por meio do set, recebendo valores do controller

---
+++ src/main/java/com/wictor/model/User.java
@@ -0,0 +1,133 @@
+package com.wictor.model;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private String cpf;
+    private String senha;
+    private LocalDate datanasc;
+    private String nome;
+    private String email1;
+    private String email2;
+    private String tel1;
+    private String tel2;
+    private String cep;
+    private String bairro;
+    private String rua;
+    private String num;
+    private String comp;
+    private String sexo;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDatanasc() {
+        return datanasc;
+    }
+
+    public void setDatanasc(LocalDate datanasc) {
+        this.datanasc = datanasc;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+    public String getTel1() {
+        return tel1;
+    }
+
+    public void setTel1(String tel1) {
+        this.tel1 = tel1;
+    }
+
+    public String getTel2() {
+        return tel2;
+    }
+
+    public void setTel2(String tel2) {
+        this.tel2 = tel2;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getComp() {
+        return comp;
+    }
+
+    public void setComp(String comp) {
+        this.comp = comp;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
